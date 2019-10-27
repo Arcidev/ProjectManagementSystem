eacd1cb1cf951e2c6521b811d3f8abd5d2ae07c1
Arci <Arcidev@users.noreply.github.com>
2019-10-27 22:40:08 +0100

    RestAPI: Finished project/task controllers

--- src/ProjectManagementSystem/Controllers/ProjectController.cs
+++ src/ProjectManagementSystem/Controllers/ProjectController.cs
@@ -16,10 +16,35 @@ namespace ProjectManagementSystem.Controllers
             this.projectFacade = projectFacade;
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}")]
         public async Task<ProjectDTO> Get(int id)
         {
             return await projectFacade.GetProject(id);
+        }
+
+        [HttpGet("{code}")]
+        public async Task<ProjectDTO> Get(string code)
+        {
+            return await projectFacade.GetProject(code);
+        }
+
+        [HttpPost]
+        public async Task<ProjectDTO> Add(ProjectDTO project)
+        {
+            await projectFacade.AddProject(project);
+            return project;
+        }
+
+        [HttpPut]
+        public async Task Update(ProjectDTO project)
+        {
+            await projectFacade.UpdateProject(project);
+        }
+
+        [HttpDelete]
+        public async Task Delete(int id)
+        {
+            await projectFacade.DeleteProject(id);
         }
     }
 }
---
+++ src/ProjectManagementSystem/Controllers/TaskController.cs
@@ -0,0 +1,51 @@
+﻿using BL.DTO;
+using BL.Facades;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private readonly TaskFacade taskFacade;
+
+        public TaskController(TaskFacade taskFacade)
+        {
+            this.taskFacade = taskFacade;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<TaskDTO> Get(int id)
+        {
+            return await taskFacade.GetTask(id);
+        }
+
+        [HttpGet("project/{projectId}")]
+        public async Task<IEnumerable<TaskDTO>> GetByProject(int projectId)
+        {
+            return await taskFacade.GetTasksByProject(projectId);
+        }
+
+        [HttpPost]
+        public async Task<TaskDTO> Add(TaskDTO task)
+        {
+            await taskFacade.AddTask(task);
+            return task;
+        }
+
+        [HttpPut]
+        public async Task Update(TaskDTO task)
+        {
+            await taskFacade.UpdateTask(task);
+        }
+
+        [HttpDelete]
+        public async Task Delete(int id)
+        {
+            await taskFacade.DeleteTask(id);
+        }
+    }
+}
